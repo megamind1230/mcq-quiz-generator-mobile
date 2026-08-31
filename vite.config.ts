22863dbc530ef01aba005008843d4ca89591a46f
megamind1230 <hassan24641@gmail.com>
2026-08-31 22:19:15 +0300

    github actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     VitePWA({
@@ -15,7 +16,7 @@ export default defineConfig({
         theme_color: '#7c0aed',
         background_color: '#0f172a',
         display: 'standalone',
-        start_url: '/',
+        start_url: './',
         icons: [
           { src: 'icons/icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' }
